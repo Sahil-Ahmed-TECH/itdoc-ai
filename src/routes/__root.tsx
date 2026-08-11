9d9c584966fff654dffad1c65f5fd45d18769f06
Ahmed <Sahil.Ahmed-TECH@protonmail.com>
2026-08-11 14:20:33 +0530

    Refine app to default dark theme

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -133,7 +133,7 @@ function RootComponent() {
     <QueryClientProvider client={queryClient}>
       {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
       <Outlet />
-      <Toaster position="top-right" richColors />
+      <Toaster position="top-right" richColors theme="dark" />
     </QueryClientProvider>
   );
 }
