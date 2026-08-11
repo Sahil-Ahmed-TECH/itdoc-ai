7b375011580bad924e48890f862f448880e8cdaa
Ahmed <Sahil.Ahmed-TECH@protonmail.com>
2026-08-11 14:38:01 +0530

    Updated __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -133,7 +133,7 @@ function RootComponent() {
     <QueryClientProvider client={queryClient}>
       {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
       <Outlet />
-      <Toaster position="top-right" richColors theme="dark" />
+      <Toaster position="top-right" theme="dark" />
     </QueryClientProvider>
   );
 }
